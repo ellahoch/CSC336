c9df5a45f1647925192f103dba573a378bef1cfd
ellahoch <eh2149a@american.edu>
2024-10-01 19:31:57 -0400

    a lot of files

---
+++ lecture5/cmtoin.js
@@ -0,0 +1,7 @@
+function cmtoin() {
+    let conversion = 2.54;
+    let length = 39;
+    let inlength = length * conversion;
+    document.write("Length in inches: " + length + "\n");
+    document.write("\n length in cm: " + inlength);
+}
---
+++ lecture5/firstExample.js
@@ -0,0 +1,7 @@
+function firstAlert() {
+    alert("hello world welcome to web programming 1");
+    // alert("page title is " + document.title);
+}
+
+let name = "Ella";
+
